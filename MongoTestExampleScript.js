9cbb2b3aa4407369f006e53841d2f0d6f7957225
ElroyCrasto <elroycrastopaul@gmail.com>
2024-01-16 17:03:06 +0530

    Minor Optimizations

--- MongoTestExampleScript.js
+++ MongoTestExampleScript.js
@@ -1,39 +1,47 @@
-const mongoose = require("mongoose")
-const UserSchema  = require("./models/User")
-const User = mongoose.model("User", UserSchema)
-mongoose.connection.on("connected",()=>{console.log("AWP Databse Connected: True")})
+//Imports
+const mongoose = require("mongoose");
+const UserSchema = require("./models/User");
+const User = mongoose.model("User", UserSchema);
+
+//Database Connection CallBack Function
+mongoose.connection.on("connected", () => {
+  console.log("AWP Databse Connected: True");
+});
+
+//Establishing Database Connection
 try {
-    mongoose.set('strictQuery', false)
-    mongoose.connect("mongodb://localhost/AWP")
-    console.log('Mongo connected')
-}catch(error) {
-    console.log(error)
-    process.exit()
-}
-//console.log(User)
-run()
-async function run(){
-    //User Details
-    var user1 = new User({
-        FirstName : "Test",
-        LastName: "User",
-        Email : "TestUser@Testing.com",
-        Password : "TestPass",
-        PhoneNumber : "0123456789",
-    })
-
-    //Saving to Database
-    try{
-        await user1.save()
-    }catch (e){
-        console.log(e.message)
-    }
-
-    //Displaying User & Password Verification
-    console.log(user1)
-    console.log(await user1.VerifyPassword("0123456789"))
-    
-    //Exit
-    process.exit()
+  mongoose.set("strictQuery", false);
+  mongoose.connect("mongodb://localhost/AWP");
+  console.log("Mongo connected");
+} catch (error) {
+  console.log(error);
+  process.exit();
 }
 
+async function run() {
+  //User Details
+  var user1 = new User({
+    FirstName: "Test",
+    LastName: "User",
+    Email: "TestUser@Testing.com",
+    Password: "TestPass",
+    PhoneNumber: "0123456789",
+  });
+
+  //Saving to Database
+  try {
+    await user1.save();
+  } catch (e) {
+    console.log(e.message);
+  }
+
+  //Displaying User & Password Verification
+  console.log(user1);
+  console.log(await user1.VerifyPassword("0123456789"));
+
+  //Exit
+  process.exit();
+}
+
+//ScriptExec
+run();
