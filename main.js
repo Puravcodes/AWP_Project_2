8ba647eb4e53a5a7cc3b4ef5e28dee7809e06dd7
Leroy Moraes <lvmoraes223@gmail.com>
2024-01-25 17:30:04 +0530

    updated main.js file and added routes for login, signup and profilepage

--- main.js
+++ main.js
@@ -4,15 +4,23 @@ var port = process.env.PORT||8080;
 var morgan = require('morgan');
 var mongoose = require('mongoose');
 var router = express.Router();
-var appRoutes = require('./website/routes/API')(router);
 var path = require('path');
+const absolutePath = path.resolve(__dirname); 
 
 
 app.use(morgan('dev')); // logs request in terminal
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true })); // decodes json data to text 
 app.use(express.static(path.join(__dirname + '/website/templates')));
-app.use('/api',appRoutes);
+
+//loading each folder from assets folder
+app.use('/assets', express.static(path.join(absolutePath, 'assets')));
+
+//app.use('/assets/css', express.static(path.join(absolutePath, '/css')));
+//app.use('/assets/img', express.static(path.join(absolutePath, '/img')));
+//app.use('/assets/js', express.static(path.join(absolutePath, '/js')));
+//app.use('/assets/scss', express.static(path.join(absolutePath, '/scss')));
+//app.use('/assets/vendor', express.static(path.join(absolutePath, '/vendor')));
 
 
 async function connectToDatabase(){
@@ -28,9 +36,22 @@ async function connectToDatabase(){
 };
 connectToDatabase();
 
-app.get('*', function(req,res){
+app.get('/', function(req,res){
   res.sendFile(path.join(__dirname + '/website/templates/index.html'));
 });
+
+app.get('/login', function(req,res){
+  res.sendFile(path.join(__dirname+'/website/templates/'));
+});
+
+app.get('/signup',function(req,res){
+  res.sendFile(path.join(__dirname+'/website/templates/'));
+});
+
+app.get('/profile',function(req,res){
+  res.sendFile(path.join(__dirname+'/website/templates/profilePage.html'));
+});
+
 
 app.listen(port, function(){
     console.log('Running server on port '+port);
