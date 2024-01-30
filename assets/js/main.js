cc141855b45025ec97bd69db0434bf9d991f3128
Leroy Moraes <lvmoraes223@gmail.com>
2024-01-30 14:50:54 +0530

    new main.js validation

--- assets/js/main.js
+++ assets/js/main.js
@@ -280,6 +280,22 @@
   });
 
   /**
+   * Initiate Bootstrap validation check
+   */
+  var needsValidation = document.querySelectorAll('.needs-validation')
+
+  Array.prototype.slice.call(needsValidation)
+    .forEach(function(form) {
+      form.addEventListener('submit', function(event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+
   /**
    * Initiate Datatables
    */
